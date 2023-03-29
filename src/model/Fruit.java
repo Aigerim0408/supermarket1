0d02bd9a4e57ed9c11cd6c6f7cf33f3be4c4e10d
beka3301 <bekturhanazarov98@gmail.com>
2023-03-29 19:42:29 +0600

    update commit

--- src/model/Fruit.java
+++ src/model/Fruit.java
@@ -1,4 +1,41 @@
 package model;
 
 public class Fruit {
+    private String name;
+    private int price;
+    private int qolich;
+
+    public Fruit(String name, int price, int qolich) {
+        this.name = name;
+        this.price = price;
+        this.qolich = qolich;
+    }
+
+    public Fruit() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQolich() {
+        return qolich;
+    }
+
+    public void setQolich(int qolich) {
+        this.qolich = qolich;
+    }
+
 }
